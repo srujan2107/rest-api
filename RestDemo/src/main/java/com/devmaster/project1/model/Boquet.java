00882930ff073ff966b5bcc34774f83f53bf0669
srujan2107 <annam@SANTU>
2016-12-28 22:53:32 -0600

    added boquet

--- RestDemo/src/main/java/com/devmaster/project1/model/Boquet.java
+++ RestDemo/src/main/java/com/devmaster/project1/model/Boquet.java
@@ -2,4 +2,22 @@ package com.devmaster.project1.model;
 
 public class Boquet {
 
+	private Integer boquetId;
+	private String boquetDesc;
+	
+	public Integer getBoquetId() {
+		return boquetId;
+	}
+	public void setBoquetId(Integer boquetId) {
+		this.boquetId = boquetId;
+	}
+	public String getBoquetDesc() {
+		return boquetDesc;
+	}
+	public void setBoquetDesc(String boquetDesc) {
+		this.boquetDesc = boquetDesc;
+	}
+	
+	
+	
 }
